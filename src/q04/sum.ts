8fababad50fd529ad18f0749aa18ca7497be2bd3
surawitpakeepol <surawitpakeepol@gmail.com>
2025-12-15 10:21:27 +0700

    sdfa

--- src/q04/sum.ts
+++ src/q04/sum.ts
@@ -1,10 +1,10 @@
-const input = Number(process.argv[2]);
-
-if (Number.isNaN(input) || input <= 0) {
+const input = process.argv[2];
+const point = Number(input);
+if (Number.isNaN(point) || input === "" || point < 0) {
     console.log("Invalid Input");
 } else {
     let sum = 0;
-    for (let i = 1; i <= input; i++) {
+    for (let i = 1; i <= point; i++) {
         sum += i;
     }
     console.log(sum);
